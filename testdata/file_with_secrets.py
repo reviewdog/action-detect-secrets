964fcd6d63c392f8ba336ac20dbefd1aecef2cb3
Pavlo Bashynskiy <levonet@gmail.com>
2021-03-22 18:13:48 +0200

    test

--- testdata/file_with_secrets.py
+++ testdata/file_with_secrets.py
@@ -3,8 +3,16 @@
 SUPER_SECRET_VALUES = '60b725f10c9c85c70d97880dfe8191a3', '3b5d5c3712955042212316173ccf37de'
 
 
+def connect(host, tokens)
+    print(tokens[1])
+
+
+def init():
+    SUPER_SECRET_VALUES = '60b72ff10c9c85c70d97880dfe8191a3', '3b5d5b3712955042212316173ccf37de'
+    connect('localhost', SUPER_SECRET_VALUES)
+
 def main():
-    print('Hello world!')
+    print('boo')
 
 
 if __name__ == '__main__':
