bd37e2fe2ea3542983cb289922b54791b2977b7a
Pavlo Bashynskiy <levonet@gmail.com>
2021-03-22 17:44:59 +0200

    show env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+set -eux
+
 cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 detect-secrets --version
+
+echo detect-secrets scan ${INPUT_DETECT_SECRETS_FLAGS}
 
 detect-secrets scan ${INPUT_DETECT_SECRETS_FLAGS} \
     | baseline2rdf \
@@ -15,3 +19,5 @@ detect-secrets scan ${INPUT_DETECT_SECRETS_FLAGS} \
         -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
         -level="${INPUT_LEVEL}" \
         ${INPUT_REVIEWDOG_FLAGS}
+
+env | sort
