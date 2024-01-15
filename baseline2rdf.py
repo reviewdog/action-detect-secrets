9a65488a293e53c2ea5df5be64ca20b5035f8038
SoaAlex <l.soaresalex@gmail.com>
2024-01-15 12:57:37 +0100

    feat(action): add `skip_audited` and `verbose` options to `baseline2rdf`

--- baseline2rdf.py
+++ baseline2rdf.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import json
+import argparse
 
 rdjson = {
     'source': {
@@ -13,7 +14,7 @@ rdjson = {
 }
 
 
-def main():
+def main(skip_audited: bool = False, verbose: bool = False):
     baseline = json.load(sys.stdin)
     if not baseline['results']:
         baseline['results'] = {}
@@ -21,21 +22,25 @@ def main():
     results = {}
     for detects in baseline['results'].values():
         for item in detects:
-            key = '%s:%s' % (item['filename'], item['line_number'])
-            if key in results:
-                results[key]['message'] += '\n* ' + item['type']
+            if skip_audited and 'is_secret' in item and not item['is_secret']:
+                if verbose:
+                    print('Skipping verified secret in : %s' % item['filename'])
             else:
-                results[key] = {
-                    'message': '\n* ' + item['type'],
-                    'location': {
-                        'path': item['filename'],
-                        'range': {
-                            'start': {
-                                'line': item['line_number']
+                key = '%s:%s' % (item['filename'], item['line_number'])
+                if key in results:
+                    results[key]['message'] += '\n* ' + item['type']
+                else:
+                    results[key] = {
+                        'message': '\n* ' + item['type'],
+                        'location': {
+                            'path': item['filename'],
+                            'range': {
+                                'start': {
+                                    'line': item['line_number']
+                                }
                             }
                         }
                     }
-                }
 
     for result in results.values():
         rdjson['diagnostics'].append(result)
@@ -50,4 +55,12 @@ def main():
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--skip-audited', dest='skip_audited', action='store_true')
+    parser.add_argument('--no-skip-audited', dest='skip_audited', action='store_false')
+    parser.set_defaults(skip_audited=False)
+    parser.add_argument('--verbose', dest='verbose', action='store_true')
+    parser.set_defaults(verbose=False)
+    args = parser.parse_args()
+
+    sys.exit(main(skip_audited=args.skip_audited, verbose=args.verbose))
